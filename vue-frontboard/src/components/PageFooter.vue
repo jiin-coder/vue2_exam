6bd46cb4145bd2d5fc86af0d3e60ba69fb4d7a40
kimjiae <fpahstiq123@naver.com>
2023-02-23 22:24:02 +0900

    Frontboard layout 분할

---
+++ vue-frontboard/src/components/PageFooter.vue
@@ -0,0 +1,17 @@
+<!-- PageFooter.vue -->
+<template>
+  <div>
+    <hr/>
+    <footer>
+      여기는 footer 자리입니다.
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
